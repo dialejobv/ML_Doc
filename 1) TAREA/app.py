9bed6e788cee742a1acd9eb836ca0946cb1f933c
Diego Barragán <dialejobv@yahoo.com>
2025-03-06 16:57:03 -0500

    Subiendo la primer tarea de machine learning

---
+++ 1) TAREA/app.py
@@ -0,0 +1,19 @@
+education_level = 0
+is_student = []
+city_of_birth = []
+temperature = 0
+area_code = 0
+favorite_sport = []
+
+print("Categórico")
+print(type(education_level))
+print("Binario")
+print(type(is_student))
+print("Categórico")
+print(type(city_of_birth))
+print("Numérico")
+print(type(temperature))
+print("Numérico")
+print(type(area_code))
+print("Categórico")
+print(type(favorite_sport))
